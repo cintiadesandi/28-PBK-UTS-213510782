59ca3943d62979bee970397e97ed114346dc2b90
cintiadesandi <131336252+cintiadesandi@users.noreply.github.com>
2023-04-20 23:54:14 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>Daftar Tugas</h1><hr><br>
     <form @submit.prevent="addActivity">
       <input type="text" v-model="newActivity" placeholder="Tambahkan Tugas...">
-      <button>Tambah</button><p>&#128516; &#9996;</p>
+      <button>Tambah</button><p>&#128516;&#9996;</p>
     </form><br>
     <table>
     <ul>
