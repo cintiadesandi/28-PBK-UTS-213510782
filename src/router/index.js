c5a21c9ff07ea69ca1e7684a464d3e39de328737
cintiadesandi <cintiadesandi@student.uir.ac.id>
2023-05-11 14:54:24 +0700

    add commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import About from '../views/HomeView.vue'
+import myapps from '../views/myapps.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+    
+  },
+  {
+    path:'/myapps',
+    name:'myapps',
+    component: myapps
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/myapps.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+      <h1>Daftar Tugas<p>&#9997;&#10071;</p></h1><hr><br>
+      <form @submit.prevent="addActivity">
+        <input type="text" v-model="newActivity" placeholder="Tambahkan Tugas...&#128516;&#9996;">
+        <button>Tambah</button>
+      </form><br>
+      <ul>
+        <li v-for="(activity, index) in filteredActivities" :key="index" :class="{ done: activity.completed }">
+          <input type="checkbox" v-model="activity.completed">
+          <span>{{ activity.text }}</span>
+          <button @click="removeActivity(index)">Hapus</button>
+        </li>
+      </ul><br>
+      <div>
+        <label>Tampilkan Tugas yang Belum Selesai:</label>
+        <input type="checkbox" v-model="showUnfinished">
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newActivity: '',
+        activities: [
+          { text: 'Makalah DAA', completed: false }, 
+          { text: 'UTS PBK', completed: true }, 
+          { text: 'Makalah Jarkom', completed: false } 
+        ],
+        showUnfinished: false
+      }
+    },
+    methods: {
+      addActivity() {
+        if (this.newActivity.trim() !== '') {
+          this.activities.push({ text: this.newActivity, completed: false });
+          this.newActivity = '';
+        }
+      },
+      removeActivity(index) {
+        this.activities.splice(index, 1);
+      }
+    },
+    computed: {
+      filteredActivities() {
+        if (this.showUnfinished) {
+          return this.activities.filter(activity => !activity.completed);
+        } else {
+          return this.activities;
+        }
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  .done {
+    text-decoration: line-through;
+    text-decoration-color: yellow;
+    font-style: italic;
+  }
+  h1 {
+    color: blue;
+    font-style: oblique;
+    text-align: center;
+  }
+  .addActivity {
+    text-decoration-color: red;
+  }
+  </style>
